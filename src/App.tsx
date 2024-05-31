32a2d3151681f37cf0cd6d66e4021f9d7f5ace1c
Álvaro García <alvaro@complexchaos.ai>
2024-05-31 19:59:57 +0100

    APP.TSX - Fetchs users - Lists five first - Add new user to the list

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+
 import './App.css';
 
+import { getUsers } from './services/userService';
+
+
+
 function App() {
+  type User = {
+    id: number;
+    name: string;
+  }
+
+  const [users, setUsers] = useState<User[]>([]);
+  const [newUser, setNewUser] = useState<string>('')
+
+  useEffect(() => {
+    getUsers().then((response) => {
+      return response.json();
+    }).then((data) => {
+      setUsers(data.slice(0, 5));
+    })
+  }, []);
+
+  const addUser = (name: string) => {
+    setUsers((prev: any) => [...prev, { id: users.length + 1, name }])
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <main>
+        <h1>Users</h1>
+        <ul>
+          {users.map((user: any) => (
+            <li key={user.id}>{user.id}: {user.name}</li>
+          ))}
+        </ul>
+
+        <div>
+          <input type="text" onChange={(e) => {
+            setNewUser(e.target.value);
+          }
+          } />
+          <button onClick={() => addUser(newUser)}>Add User</button>
+        </div>
+      </main >
+    </div >
   );
 }
+
+
 
 export default App;
